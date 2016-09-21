f8de9f78de5107bd3bf40a89ac8c33c07bdbaa9f
vicente.garcia <vbgarciag@gmail.com>
2016-09-20 22:56:01 -0500

    inicializando el repositorio

--- CodeIgniter/application/views/index.php
+++ CodeIgniter/application/views/index.php
@@ -5,11 +5,11 @@
 	<meta name="viewreport" content="width=device-width, initial-scale=1">
 	<title>Bootstrap</title>
 
-	<link rel="stylesheet" media="screen" href="css/bootstrap.min.css">
-	<script src="js/jquery-3.0.0.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-	<script src="js/style_index.js"></script>
-	<link rel="stylesheet" type="text/css" href="css/style.css">
+	
+	<link rel="stylesheet" media="screen" href="assets/bootstrap/css/bootstrap.min.css">
+	
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	
 	
 </head>
 <body>
@@ -24,15 +24,19 @@
       			<button type="submit" class="btn btn-warning">Create Account</button>
       			</div>
     		</form>
-    		<a class="navbar-form navbar-left" rel="home" href="index.php" ><img style="max-width:100px; -7px" src="images/logo.png"></a>	
+    		<a class="navbar-form navbar-left" rel="home" href="index.php" ><img style="max-width:100px; -7px" src="assets/images/logo.png"></a>	
     	</div>
 	</nav>
 
 	<div class="bs-example">
 		<div class="img">
-			<img src="images/nube.jpg" class="img-rounded" data-toggle="popover" data-content="We are your best option!">
+			<img src="assets/images/nube.jpg" class="img-rounded" data-toggle="popover" data-content="We are your best option!">
 		</div>
 		<center><h2>Suite Document!</h2></center>
 	</div>
 </body>
+	<script src="assets/bootstrap/js/jquery-3.0.0.min.js"></script>
+
+	<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+	<script src="assets/js/style_index.js"></script>
 </html>	
