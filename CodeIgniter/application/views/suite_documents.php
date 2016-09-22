cc0e18f50a34184fad4fccb1fc09d57d98c5eeee
leninangulo95 <leninangulo.0309@gmail.com>
2016-09-22 18:39:29 -0500

    se creo el archivo suite_documents.php

---
+++ CodeIgniter/application/views/suite_documents.php
@@ -0,0 +1,55 @@
+<html>
+<head>
+	<meta charset= "utf-8">
+	<meta name="viewreport" content="width=device-width, initial-scale=1">
+	<title>Bootstrap</title>
+
+	<link rel="stylesheet" media="screen" href="css/bootstrap.min.css">
+
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+	<link rel="stylesheet" type="text/css" href="css/style_cuenta.css">
+
+	<body>
+		<nav class="navbar navbar-inverse" role="navigation">
+			<nav class="navbar navbar-inverse navbar-fixed-bottom"></nav>
+			<div class="collapse navbar-collapse navbar-ex1-collapse">
+
+
+				<form class="navbar-form navbar-right" role="search">
+					<div class="btn">
+						<a href="#" class="btn btn-danger btn-lg">Log-Out</a>
+					</div>
+
+				</form>
+				<a class="navbar-form navbar-left" rel="home" href="index.php" ><img style="max-width:100px; -7px" src="images/logo.png"></a>	
+			</nav>
+
+			<nav class="navbar navbar-default sidebar" role="navigation">
+				<div class="container-fluid">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-sidebar-navbar-collapse-1">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>      
+					</div>
+					<div class="collapse navbar-collapse" id="bs-sidebar-navbar-collapse-1">
+						<ul class="nav navbar-nav">
+						<li><a href="#">Home<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-home"></span></a></li>
+
+						</li>          
+						<li ><a href="#">New File<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-folder-open"></span></a></li>        
+						<li ><a href="#">Send Email<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-envelope"></span></a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+		<script src="assets/bootstrap/js/jquery-3.0.0.min.js"></script>
+
+		<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+		<script src="assets/js/style_index.js"></script>
+
+	</body>
+	</html>
